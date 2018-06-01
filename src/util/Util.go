b29b0bb217acef4d10dd88e76c11f9eab43de2f7
ZhengXing <http://121.40.176.35:7777/buc/framework>
2018-06-01 20:14:34 +0800

    继续学习

--- src/util/Util.go
+++ src/util/Util.go
@@ -5,8 +5,10 @@ import "log"
 /*工具类*/
 
 /*判断是否有异常，有则打印日志*/
-func LogError(err error,v ...interface{}) {
+func LogError(err error,v ...interface{}) bool{
 	if err != nil {
 		log.Fatalln(v)
+		return false
 	}
+	return true
 }
